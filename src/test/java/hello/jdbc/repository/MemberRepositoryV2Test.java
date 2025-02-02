91321a8249b82ae6ed4f729b208af8e82b984392
master <ted3131@diquest.com>
2025-02-03 08:19:10 +0900

    commit

--- src/test/java/hello/jdbc/repository/MemberRepositoryV2Test.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV2Test.java
@@ -36,15 +36,15 @@ class MemberRepositoryV2Test {
     @Test
     @DisplayName("정상 이체")
     void accountTransfer() throws SQLException {
-//given
+        //given
         Member memberA = new Member("memberA", 10000);
         Member memberB = new Member("memberB", 10000);
         memberRepository.save(memberA);
         memberRepository.save(memberB);
-//when
+        //when
         memberService.accountTransfer(memberA.getMemberId(),
                 memberB.getMemberId(), 2000);
-//then
+        //then
         Member findMemberA = memberRepository.findById(memberA.getMemberId());
         Member findMemberB = memberRepository.findById(memberB.getMemberId());
         assertThat(findMemberA.getMoney()).isEqualTo(8000);
@@ -53,21 +53,21 @@ class MemberRepositoryV2Test {
     @Test
     @DisplayName("이체중 예외 발생")
     void accountTransferEx() throws SQLException {
-//given
+        //given
         Member memberA = new Member("memberA", 10000);
         Member memberEx = new Member("ex", 10000);
         memberRepository.save(memberA);
         memberRepository.save(memberEx);
-//when
+        //when
         assertThatThrownBy(() ->
                 memberService.accountTransfer(memberA.getMemberId(), memberEx.getMemberId(),
                         2000))
                 .isInstanceOf(IllegalStateException.class);
-//then
+        //then
         Member findMemberA = memberRepository.findById(memberA.getMemberId());
         Member findMemberEx =
                 memberRepository.findById(memberEx.getMemberId());
-//memberA의 돈이 롤백 되어야함
+        //memberA의 돈이 롤백 되어야함
         assertThat(findMemberA.getMoney()).isEqualTo(10000);
         assertThat(findMemberEx.getMoney()).isEqualTo(10000);
     }
