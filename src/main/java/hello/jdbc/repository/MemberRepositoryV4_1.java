4ac57914c696f7a4eb10582f3edbf0e5199f783e
master <ted3131@diquest.com>
2025-02-06 16:29:15 +0900

    commit

--- src/main/java/hello/jdbc/repository/MemberRepositoryV4_1.java
+++ src/main/java/hello/jdbc/repository/MemberRepositoryV4_1.java
@@ -1,24 +1,29 @@
 package hello.jdbc.repository;
 
 import hello.jdbc.domain.Member;
+import hello.jdbc.repository.ex.MyDbException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.datasource.DataSourceUtils;
 import org.springframework.jdbc.support.JdbcUtils;
+
 import javax.sql.DataSource;
 import java.sql.*;
 import java.util.NoSuchElementException;
+
 /**
- * 트랜잭션 - 트랜잭션 매니저
- * DataSourceUtils.getConnection()
- * DataSourceUtils.releaseConnection()
+ * 예외 누수 문제 해결
+ * 체크 예외를 런타임 예외로 변경
+ * MemberRepository 인터페이스 사용
+ * throws SQLException 제거
  */
 @Slf4j
-public class MemberRepositoryV3 implements MemberRepositoryEX {
+public class MemberRepositoryV4_1 implements MemberRepository {
     private final DataSource dataSource;
-    public MemberRepositoryV3(DataSource dataSource) {
+    public MemberRepositoryV4_1(DataSource dataSource) {
         this.dataSource = dataSource;
     }
-    public Member save(Member member) throws SQLException {
+    @Override
+    public Member save(Member member) {
         String sql = "insert into member(member_id, money) values(?, ?)";
         Connection con = null;
         PreparedStatement pstmt = null;
@@ -30,13 +35,13 @@ public class MemberRepositoryV3 implements MemberRepositoryEX {
             pstmt.executeUpdate();
             return member;
         } catch (SQLException e) {
-            log.error("db error", e);
-            throw e;
+            throw new MyDbException(e);
         } finally {
             close(con, pstmt, null);
         }
     }
-    public Member findById(String memberId) throws SQLException {
+    @Override
+    public Member findById(String memberId) {
         String sql = "select * from member where member_id = ?";
         Connection con = null;
         PreparedStatement pstmt = null;
@@ -56,13 +61,13 @@ public class MemberRepositoryV3 implements MemberRepositoryEX {
                         + memberId);
             }
         } catch (SQLException e) {
-            log.error("db error", e);
-            throw e;
+            throw new MyDbException(e);
         } finally {
             close(con, pstmt, rs);
         }
     }
-    public void update(String memberId, int money) throws SQLException {
+    @Override
+    public void update(String memberId, int money) {
         String sql = "update member set money=? where member_id=?";
         Connection con = null;
         PreparedStatement pstmt = null;
@@ -73,13 +78,13 @@ public class MemberRepositoryV3 implements MemberRepositoryEX {
             pstmt.setString(2, memberId);
             pstmt.executeUpdate();
         } catch (SQLException e) {
-            log.error("db error", e);
-            throw e;
+            throw new MyDbException(e);
         } finally {
             close(con, pstmt, null);
         }
     }
-    public void delete(String memberId) throws SQLException {
+    @Override
+    public void delete(String memberId) {
         String sql = "delete from member where member_id=?";
         Connection con = null;
         PreparedStatement pstmt = null;
@@ -89,8 +94,7 @@ public class MemberRepositoryV3 implements MemberRepositoryEX {
             pstmt.setString(1, memberId);
             pstmt.executeUpdate();
         } catch (SQLException e) {
-            log.error("db error", e);
-            throw e;
+            throw new MyDbException(e);
         } finally {
             close(con, pstmt, null);
         }
@@ -98,11 +102,9 @@ public class MemberRepositoryV3 implements MemberRepositoryEX {
     private void close(Connection con, Statement stmt, ResultSet rs) {
         JdbcUtils.closeResultSet(rs);
         JdbcUtils.closeStatement(stmt);
-        //주의! 트랜잭션 동기화를 사용하려면 DataSourceUtils를 사용해야 한다.
         DataSourceUtils.releaseConnection(con, dataSource);
     }
-    private Connection getConnection() throws SQLException {
-        //주의! 트랜잭션 동기화를 사용하려면 DataSourceUtils를 사용해야 한다.
+    private Connection getConnection() {
         Connection con = DataSourceUtils.getConnection(dataSource);
         log.info("get connection={} class={}", con, con.getClass());
         return con;
